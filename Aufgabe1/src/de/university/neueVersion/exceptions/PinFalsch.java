82cd11f2c943fa36799eb6397423e37d9238544b
malcjohn <cincenko@seznam.cz>
2013-03-24 21:44:59 +0100

    udpate

---
+++ Aufgabe1/src/de/university/neueVersion/exceptions/PinFalsch.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package de.university.neueVersion.exceptions;
+
+/**
+ * @author Dima
+ *
+ */
+public class PinFalsch extends Exception {
+	
+	public PinFalsch() {
+		System.out.println("");
+		System.out.println("Sie konnen die Informationen nicht kriegen, weil der Pin falsch ist");
+	}
+
+}
