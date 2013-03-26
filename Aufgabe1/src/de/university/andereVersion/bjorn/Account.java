d24625aee45a7dc03415a138910132ffb1c809ed
malcjohn <cincenko@seznam.cz>
2013-03-26 20:54:15 +0100

    bjorn

--- Aufgabe1/src/de/university/andereVersion/bjorn/Account.java
+++ Aufgabe1/src/de/university/andereVersion/bjorn/Account.java
@@ -4,11 +4,13 @@ public class Account {
 	public int accountNumber;
 	public double overdraft;
 	public double bankDeposit;
+	public int pin;
 
 	public Account() {
 		accountNumber = 0;
 		overdraft = 0.0;
 		bankDeposit = 0.0;
+		pin = 0;
 	}
 
 	// Get Methoden
@@ -24,6 +26,10 @@ public class Account {
 		return this.bankDeposit;
 	}
 
+	public int getPin() {
+		return this.pin;
+	}
+
 	// Set Methoden
 	public void setAccountNumber(int newAccountNumber) {
 		this.accountNumber = newAccountNumber;
@@ -35,5 +41,9 @@ public class Account {
 
 	public void setBankDeposit(double newBankDeposit) {
 		this.bankDeposit = newBankDeposit;
+	}
+
+	public void setPin(int newPin) {
+		this.pin = newPin;
 	}
 }
--- Aufgabe1/src/de/university/andereVersion/bjorn/CashMachine.java
+++ Aufgabe1/src/de/university/andereVersion/bjorn/CashMachine.java
@@ -1,13 +1,14 @@
 package de.university.andereVersion.bjorn;
 
+import java.util.*;
+
 public class CashMachine {
 	public enum Zustand {
-		READY, CARD_INSERTED;
+		READY, CARD_INSERTED, PIN_CORRECT, PIN_WRONG;
 	}
 
 	Zustand eins;
-	// Account[] accounts = new Account[10];
-	// CashCard[] cashCard = new CashCard[10];
+	Zustand zwei;
 	CashCard cashCard = new CashCard();
 	public String status;
 	Account aEins = new Account();
@@ -21,18 +22,21 @@ public class CashMachine {
 	Account aNeun = new Account();
 	Account aZehn = new Account();
 	Account inBearbeitung = new Account();
+	Scanner enter = new Scanner(System.in);
 
 	Account[] accounts = { aEins, aZwei, aDrei, aVier, aFuenf, aSechs, aSieben,
 			aAcht, aNeun, aZehn };
 
 	public CashMachine() {
-		this.eins = eins.READY;
+		this.eins = Zustand.READY;
+		this.zwei = Zustand.PIN_WRONG;
 		status = "READY";
 	}
 
 	public void insertCashCard(CashCard cashCarde) {
-		if (eins == eins.READY) {
-			eins = eins.CARD_INSERTED;
+		switch (eins) {
+		case READY:
+			eins = Zustand.CARD_INSERTED;
 			status = "CARD INSERTED";
 			System.out.println("Status : " + status);
 			System.out.println("Willkommen");
@@ -42,39 +46,71 @@ public class CashMachine {
 				if (cashCard.getAccountNumber() == accounts[i]
 						.getAccountNumber()) {
 					inBearbeitung = accounts[i];
+					System.out.println("Konto gefunden!");
 
 				} // end of if
 
 			} // end of for
 
+			if (inBearbeitung.getAccountNumber() == 0) {
+				System.out.println("");
+				System.out
+						.println("Fehler EC-Karte konnte keinem Konto zugewiesen werden");
+				System.exit(-1);
+			} // end of if-else
+
+			break;
+
+		case CARD_INSERTED:
+			System.out.println("Karte ist bereits eingelesen!");
+			break;
+
 		}
 
-		else {
-			System.out.println("Kartenfehler! Automat nicht bereit");
-
-		}
-	}
+	} // Methodenende
 
 	public void withdraw(double amount) {
-		if (eins == eins.CARD_INSERTED) {
-			if (amount <= inBearbeitung.getOverdraft()) {
-				inBearbeitung.bankDeposit = (inBearbeitung.bankDeposit - amount);
-				System.out.println("Ihr Neuer Kontostand betraegt : "
-						+ inBearbeitung.getBankDeposit() + " EUR");
+		switch (eins) {
+		case CARD_INSERTED:
+
+			switch (zwei) {
+			case PIN_CORRECT:
+
+				if (amount <= (inBearbeitung.getOverdraft() + inBearbeitung
+						.getBankDeposit())) {
+					inBearbeitung.bankDeposit = (inBearbeitung.bankDeposit - amount);
+					System.out.println("Ihr Neuer Kontostand betraegt : "
+							+ inBearbeitung.getBankDeposit() + " EUR");
+
+				}
+
+				else {
+					System.out
+							.println("Der Angeforderte Betrag ueberschreitet ihr Dispolimit! Keine Auszahlung moeglich");
+
+				} // end of if-else
+				break;
+
+			case PIN_WRONG:
+
+				System.out.println("");
+				System.out.println("Pin nicht Korrekt!");
+				break;
 
 			}
 
-			else {
-				System.out
-						.println("Der Angeforderte Betrag ueberschreitet ihr Dispolimit! Keine Auszahlung moeglich");
+		case READY:
 
-			} // end of if-else
+			System.out.println("");
+			System.out.println("Fehler keine Karte eingelesen!");
 
 		}
 	}
 
 	public void accountStatement() {
-		if (eins == eins.CARD_INSERTED) {
+		switch (eins) {
+		case CARD_INSERTED:
+
 			System.out.println("Ihre Aktuellen Kontodaten lauten : ");
 			System.out.println();
 			System.out.println("Kontonummer  : "
@@ -83,18 +119,78 @@ public class CashMachine {
 					+ inBearbeitung.getBankDeposit());
 			System.out
 					.println("Dispokredit  : " + inBearbeitung.getOverdraft());
+			System.out.println("");
+			eins = Zustand.READY;
+			break;
+
+		case READY:
+
+			System.out.println("");
+			System.out.println("Bitte erst Karte einlesen!");
+
 		}
 	}
 
 	public void ejectCashCard() {
-		if (eins == eins.CARD_INSERTED) {
+		switch (eins) {
+		case CARD_INSERTED:
+
 			cashCard = null;
+			eins = Zustand.READY;
+			status = "READY";
+			System.out.println("Status : " + status);
+			System.out.println("Auf Widersehen");
+			break;
+
+		case READY:
+
+			System.out.println("");
+			System.out
+					.println("Fehler es befindet sich keine Karte im Automaten");
+			break;
+
 		}
 
-		eins = eins.READY;
-		status = "READY";
-		System.out.println("Status : " + status);
-		System.out.println("Auf Widersehen");
+	}
+
+	public void enterPin(int pinEingabe) {
+		boolean ok = false;
+
+		while (ok != true) {
+
+			System.out.println("");
+			System.out.println("Bitte Pin eingeben: ");
+
+			try {
+
+				pinEingabe = enter.nextInt();
+
+				if (enter.hasNextInt()) {
+					ok = true;
+				}
+
+			}
+
+			catch (InputMismatchException h) {
+				System.out.println("");
+				System.out
+						.println("Fehler! Es wurde kein gültiger Pin eingegeben");
+				System.out.println("");
+				enter.next();
+
+			}
+
+		}
+
+		if (inBearbeitung.getPin() == pinEingabe) {
+			eins = Zustand.CARD_INSERTED;
+			zwei = Zustand.PIN_CORRECT;
+
+		} // end of if
+
+		else {
+			zwei = Zustand.PIN_WRONG;
+		}
 
 	}
 
