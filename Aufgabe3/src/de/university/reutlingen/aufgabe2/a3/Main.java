886acb8e10c07094d9a450578a77950d1daf0dd0
John malc <cincenko@seznam.cz>
2013-04-10 11:35:27 +0300

    Update Main.java
    ad

--- Aufgabe3/src/de/university/reutlingen/aufgabe2/a3/Main.java
+++ Aufgabe3/src/de/university/reutlingen/aufgabe2/a3/Main.java
@@ -8,6 +8,18 @@ import de.university.reutlingen.aufgabe2.a3.ProductDescription.PriceLevel;
 public class Main {
 
 	public static void main(String[] args) {
+		/**
+		 * Das Preissystem der Warenhaussoftware soll umgestellt werden. Produktpreise sollen nicht mehr
+		 * beliebig vergeben werden können. Stattdessen gibt es 4 Preisstufen, denen ein Produkt 
+		 * zugeordnet wird. Die Stufen sind LOW (9,99), MEDIUM (19,99), HIGH (49,99) und EXCLUSIVE 
+		 * (99,99). Innerhalb der Warenhaussoftware sollen die Preisstufen durch einen Enum-Typ 
+		 * (enum PriceLevel) reprasenstiert werden. Programmieren Sie den enum PriceLevel, 
+		 * passen Sie die Klasse Product und Ihr Testprogramm aus Teilaufgabe (a) an.
+		 * Hinweis: Der Preis muss als double-Datentyp im enum PriceLevel verwaltet werden.
+		 * enum-Typ muss zusatzlich public String toString() uberschreiben (gibt den Preis
+		 * als String zuruck) und einen privaten Konstruktor besitzen, der den Preis als Argument erhalt. 
+		 */
+		
 		/*
 		 * this is a hack. Should not do, just for save heaven.
 		 * 1 Possibility
@@ -17,20 +29,17 @@ public class Main {
 		products[0] = new Produkt<String>("I Hate Apple", PriceLevel.EXCLUSIVE);
 		products[1] = new Produkt<String>("I Hate Apple", PriceLevel.LOW);
 		products[2] = new Produkt<String>("I Hate Apple", PriceLevel.MEDIUM);
-		for (Produkt<String> i : products) {
+	
+		or (Produkt<String> i : products) {
 			System.out.println(i.getProduktBeschreibung() + " "+  i.getProduktPreis());
 		}
 		
 		System.out.println("");
 		
-		/*
-		 * Teacher basically wants create arraylist and put there Product object
-		 * 2 Possibility 
-		 */
+		// This is better, because uses ArrayLists. For me
 		ArrayList<Produkt<String>> produktLists = new ArrayList<>();
 		produktLists.add(new Produkt<String>("I have bought Apple for ", PriceLevel.MEDIUM));
 		produktLists.add(new Produkt<String>("I read the Verge ", PriceLevel.LOW));
-
 
 		for (Produkt<String> g : produktLists) {
 			System.out.println(g.getProduktBeschreibung()+ " " + g.getProduktPreis());
