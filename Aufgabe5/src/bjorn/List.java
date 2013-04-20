dd086f12aaccafc0584d1461e8cef8d78b69887d
malcjohn <cincenko@seznam.cz>
2013-04-20 18:00:54 +0200

    dsfg

--- Aufgabe5/src/bjorn/List.java
+++ Aufgabe5/src/bjorn/List.java
@@ -1,16 +1,10 @@
-/**
- * 
- */
-package aufgabe4;
-
-import java.lang.*;
+package bjorn;
 
 /**
  * @author krausb
  * 
  */
-public class List<K> 
-{
+public class List<K> {
 
 	private ListNode head;
 	private ListNode temp;
@@ -20,29 +14,24 @@ public class List<K>
 	private int position = 0;
 	private int anzahlZwei = 0;
 
-	private class ListNode 
-	{
+	private class ListNode {
 		private ListNode next;
 		private K data;
 
-		public void setData(K data) 
-		{
+		public void setData(K data) {
 			this.data = data;
 		}
 
-		public void setNext(ListNode next) 
-		{
+		public void setNext(ListNode next) {
 			this.next = next;
 		}
 
 	}
 
-	public String toString() 
-	{
+	public String toString() {
 		ListNode f = head;
 		StringBuilder str = new StringBuilder();
-		while (f != null) 
-		{
+		while (f != null) {
 			str.append("<" + f.data + ">");
 			f = f.next;
 		}
@@ -51,116 +40,88 @@ public class List<K>
 
 	}
 
-	public boolean isEmpty() 
-	{
+	public boolean isEmpty() {
 		return head == null;
 
 	}
 
-	public void insertFirst(K elem) 
-	{
+	public void insertFirst(K elem) {
 		ListNode newHead = new ListNode();
 		newHead.setData(elem);
-		newHead.setNext(head);  // Bei erst erstellung ist das naechste element
-		head = newHead;			// somit null also Listenende, da die Referenz
-		anzahlZwei++;			// head oben in der Klasse nicht initialisiert
-								// wurde!
-								// bei der naechsten erstellung verweist next
-								// nun nicht mehr auf null(da nun das letzt
-								// erstellte Element die position des heads
-								// einnimmt), sondern WIEDER auf das letzt
-								// erstellte Element(da head = newHead)
-								// eingefuegtes element wird zum ersten element (head),
-								// vorheriges element rutscht an 2 stelle usw.
+		newHead.setNext(head); // Bei erst erstellung ist das naechste element
+		head = newHead; // somit null also Listenende, da die Referenz
+		anzahlZwei++; // head oben in der Klasse nicht initialisiert
+						// wurde!
+						// bei der naechsten erstellung verweist next
+						// nun nicht mehr auf null(da nun das letzt
+						// erstellte Element die position des heads
+						// einnimmt), sondern WIEDER auf das letzt
+						// erstellte Element(da head = newHead)
+						// eingefuegtes element wird zum ersten element (head),
+						// vorheriges element rutscht an 2 stelle usw.
 
 	}
-	
-	private int sizeVorbereitung()
-	{
+
+	private int sizeVorbereitung() {
 		ListNode g = head;
 
-		while (g != null) 
-		{
+		while (g != null) {
 			anzahl++;
 			g = g.next;
 
 		}
 
 		return anzahl;
-		
+
 	}
 
-	public int size() 
-	{
+	public int size() {
 		int ergebniss = sizeVorbereitung();
 		anzahl = 0;
 		return ergebniss;
-		
-		
-
 
 	}
-	
 
-	
-
-	public K get(int pos) throws IndexOutOfBoundsException 
-	{
+	public K get(int pos) throws IndexOutOfBoundsException {
 		ListNode p = head;
 
-		if (pos < 0 || pos >= size()) 
-		{
-			throw new IndexOutOfBoundsException("Fehler es wurde keine gueltige Elementposition eingegeben!");
+		if (pos < 0 || pos >= size()) {
+			throw new IndexOutOfBoundsException(
+					"Fehler es wurde keine gueltige Elementposition eingegeben!");
 		}
 
-		else 
-		{
-			while (p != null) 
-			{
-				if (position == pos)
-				{
+		else {
+			while (p != null) {
+				if (position == pos) {
 					temp = p;
-				}						
-										
+				}
+
 				position++;
-				p = p.next;						
-										
+				p = p.next;
+
 			}
-			return temp.data;							
+			return temp.data;
 		}
 	}
-	
-	
-	public void add(K elem)
-	{
-		ListNode a = head;
-		
-		
-		// Funktioniert noch nicht
-		while(a != null)
-		{
-			
-			
-			
-			if (a == null)
-			{
-				
-				ListNode newE = new ListNode();
-				newE.setData(elem);
-				a.next.setNext(newE);
-				
-				
-				
-				
-				
-				
-			}
-			
-			a = a.next;
-			
-			
-		}
-		
-	}
+
+	// public void add(K elem) {
+	// ListNode a = head;
+	//
+	// // Funktioniert noch nicht
+	// while (a != null) {
+	//
+	// if (a == null) {
+	//
+	// ListNode newE = new ListNode();
+	// newE.setData(elem);
+	// a.next.setNext(newE);
+	//
+	// }
+	//
+	// a = a.next;
+	//
+	// }
+	//
+	// }
 
 }
