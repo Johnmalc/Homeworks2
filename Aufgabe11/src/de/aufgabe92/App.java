e2e1195b56728cb8f54782c53a3a83bd27bcffca
malcjohn <cincenko@seznam.cz>
2013-06-06 17:44:49 +0200

    update

---
+++ Aufgabe11/src/de/aufgabe92/App.java
@@ -0,0 +1,25 @@
+package de.aufgabe9;
+
+public class App {
+
+	/**
+	 * beispielhaft ein Produkt erzeugt, jeweils ein Objekt der beiden
+	 * Observer-Klassen beim Produkt registriert und dann den Lagerbestand so
+	 * verringert, dass beide Observer aktiv werden mussen. Im Falle, dass zu
+	 * wenig Lagerbestand vorhanden ist soll eine RuntimeException geworfen
+	 * werden.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Produkt pr = new Produkt(12654, "Produkt Nr1", 15, 5);
+		WarehouseDisplay wd = new WarehouseDisplay();
+		WarehouseOrder wo = new WarehouseOrder();
+		pr.addObserver(wd);
+		pr.addObserver(wo);
+
+		pr.removeProducts(10);
+
+	}
+
+}
