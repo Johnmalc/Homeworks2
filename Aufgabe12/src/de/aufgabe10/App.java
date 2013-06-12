5147e7ffe02fe4a1b38873447a6895c2493288fe
malcjohn <cincenko@seznam.cz>
2013-06-12 10:36:53 +0200

    update

--- Aufgabe12/src/de/aufgabe10/App.java
+++ Aufgabe12/src/de/aufgabe10/App.java
@@ -1,25 +1,34 @@
 package de.aufgabe10;
+
 /**
  * Dmitrij Petrov Anastasia Baron
  */
 public class App {
 
 	/**
-	 * @param args
-	 *            Solution : >
-	 *            http://javapostsforlearning.blogspot.de/2012/09/composite
-	 *            -design-pattern-in-java.html
+	 * Solution : > http://javapostsforlearning.blogspot.de/2012/09/composite
+	 * -design-pattern-in-java.html
 	 */
 	public static void main(String[] args) {
-		Directory di = new Directory("C: User"); // verzeichniss
+		Directory di = new Directory("Main Dokument"); // verzeichniss
+		Directory dp = new Directory("Privat"); // verzeichniss
 
-		Entry fi = new File("index.html"); // datei
-		Entry fo = new File("index.html"); // datei
+		File fi = new File("index.html"); // datei
+		File fo = new File("index.html"); // datei
 
+		File fp = new File("SM"); // datei
+		File fr = new File("WA"); // datei
+		
 		di.add(fo);
 		di.add(fi);
 
+		dp.add(fp);
+		dp.add(fr);
+		
+		di.add(dp);
+
 		di.prinVerschachtelteStruktur();
+		dp.prinVerschachtelteStruktur();
 		System.out.println(di.numberOfEntries());
 
 	}
