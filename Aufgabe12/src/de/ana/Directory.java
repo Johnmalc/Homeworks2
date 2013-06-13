2e736ec96561ec97b36fb825f6618ea5decb3e96
Anastasia <nstushka.asya@rambler.ru>
2013-06-13 21:55:45 +0200

    Aufgabe10

---
+++ Aufgabe12/src/de/ana/Directory.java
@@ -0,0 +1,54 @@
+package de.ana;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Directory implements Entry {
+
+	public Directory(String name) {
+		this.name = name;
+	}
+
+	String name;
+	public int probel;
+	int leer = 0;
+	private List<Entry> eintraege = new LinkedList<Entry>();
+
+	public void print() {
+		for (int i = 0; i < leer; i++)
+			System.out.print(" ");
+		System.out.println(this.name);
+		leer++;
+
+		for (final Entry eintrag : eintraege) {
+			if (eintrag instanceof Directory)
+				((Directory) eintrag).leer += this.leer;
+			else if (eintrag instanceof File) {
+				for (int i = 0; i < leer; i++)
+					System.out.print(" ");
+			}
+			eintrag.print();
+		}
+	}
+
+	public int numberOfEntries() {
+		int sum = eintraege.size();
+		for (final Entry eintrag : eintraege) {
+			if (eintrag instanceof Directory)
+				sum += ((Directory) eintrag).eintraege.size();
+		}
+
+		return sum;
+	}
+
+	public void add(Entry entry) {
+
+		eintraege.add(entry);
+
+	}
+
+	public void remove(Entry entry) {
+		eintraege.remove(entry);
+	}
+}
+
