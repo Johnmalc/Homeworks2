439aadc892f555b83be1f41914fe2b9561ed9eeb
John malc <cincenko@outlook.com>
2013-06-19 10:07:17 +0200

    Update Vars.java
    letze

--- Aufgabe13/src/de/aufgabe11/Vars.java
+++ Aufgabe13/src/de/aufgabe11/Vars.java
@@ -12,7 +12,7 @@ public abstract class Vars {
 	final JButton btnNewButton_2 = new JButton("Kontostand");
 	final JButton btnNewButton_3 = new JButton("Geld abheben");
 	final JButton btnNewButton_4 = new JButton("Karte ausgeben");
-	JLabel infoPanel = new JLabel();
+	final JLabel infoPanel = new JLabel();
 	final JLabel test4 = new JLabel("Test4 ist erfolgreich. Karte raus", SwingConstants.CENTER);
 	final JLabel test4No = new JLabel("Test4 nicht bestanden. Keine Karte", SwingConstants.CENTER);
 	final JLabel test3 = new JLabel("Test3 ist erfolgreich. Pin war richtig", SwingConstants.CENTER);
@@ -20,7 +20,6 @@ public abstract class Vars {
 	final JLabel test2 = new JLabel("Test2 ist erfolgreich. Sie konnen ihren Pin setzen", SwingConstants.CENTER);
 	final JLabel test2No = new JLabel("Test2 ist nicht bestanden. falsch", SwingConstants.CENTER);	
 	final JLabel test1No = new JLabel("Test1 ist nicht bestanden. Wenig geld auf dem Konto", SwingConstants.CENTER);
-
 	final JDialog diaBu = new JDialog();
 
 	final JComboBox<Integer> comboBox = new JComboBox<Integer>(
